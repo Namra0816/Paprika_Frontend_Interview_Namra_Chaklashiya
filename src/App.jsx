ecbd0e3932e6961a3c69498e04f4c42cbcc9bffa
Namra0816 <namra.rc8216@gmail.com>
2024-07-02 23:50:20 -0700

    Hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => (
   <div className="flex flex-col bg-white">
     <Header />
+    <Hero />
   </div>
 );
 
