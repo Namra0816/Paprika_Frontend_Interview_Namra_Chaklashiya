9ba36719568c952bcd0295788f539415c3002dc2
Namra0816 <namra.rc8216@gmail.com>
2024-07-04 12:22:59 -0700

    colors, fonts updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from './components/Navbar';
 import Hero from './components/Hero';
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
 
 const App = () => (
   <div className="flex flex-col bg-white">
@@ -9,6 +10,7 @@ const App = () => (
     <Hero />
     <div className="flex gap-5 max-md:flex-col max-md:gap-0">
       <Sidebar />
+      <MainContent />
     </div>
   </div>
 );
---
+++ src/components/RestaurantHeader.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import verifiedIcon from '../assets/verifiedIcon.png';
+import addItemIllustration from '../assets/addItemIllustration.png';
+import forwardarrow from '../assets/forwardarrow.png';
+
+const RestaurantHeader = () => (
+    <div className="flex gap-12 px-px max-md:flex-wrap max-md:max-w-full">
+        <div className="flex flex-col flex-1 py-1.5">
+          <div className="flex gap-5 pr-2 mr-auto text-3xl font-bold text-stone-700">
+            <div className="text-ellipsis">The Daily Grind</div>
+            <img loading="lazy" src={verifiedIcon} className="shrink-0 my-auto aspect-square w-[35px]" alt="Verified Icon" />
+          </div>
+          <div className="mt-5 font-inter text-2xl text-offblack text-ellipsis">
+            1.2 miles away | <span className="font-bold text-emerald-500">Open</span>
+          </div>
+        </div>
+      <div className="flex flex-col flex-1 self-start p-2 bg-white rounded-2xl border border-gray-200 border-solid text-stone-700">
+        <div className="flex gap-5 items-center">
+          <img loading="lazy" src={addItemIllustration} className="shrink-0 aspect-square w-[61px]" alt="Add Icon" />
+          <div className="flex flex-col flex-1 self-stretch py-1 my-auto">
+            <div className="text-base font-bold text-ellipsis">Add Item</div>
+            <div className="mt-1 text-sm text-ellipsis">Add a new item to this menu</div>
+          </div>
+          <img loading="lazy" src={forwardarrow} className="shrink-0 my-auto aspect-square w-[22px]" alt="Plus Icon" />
+        </div>
+      </div>
+    </div>
+  );
+  
+  export default RestaurantHeader;
