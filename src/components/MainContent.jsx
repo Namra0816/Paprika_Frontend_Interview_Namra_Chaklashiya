72c3ea5ad056fe68f4d7a892350f55873e77cc05
Namra0816 <namra.rc8216@gmail.com>
2024-07-05 18:05:02 -0700

    comments added

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -7,6 +7,7 @@ const MainContent = () => (
   <main className="flex flex-col ml-5 w-4/5 max-md:ml-0 max-md:w-full">
     <div className="flex flex-col px-6 pt-10 pb-20 w-full bg-white max-md:px-5 max-md:mt-1 max-md:max-w-full">
       <RestaurantHeader />
+      {/* Divider */}
       <div className="shrink-0 mt-10 h-px border-solid bg-zinc-300 border-zinc-light max-md:max-w-full" />
       <BestMatches />
       <Appetizer />
