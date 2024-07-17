afe8508a5f2d7a820e18dad7ec4e09c485348468
Namra0816 <namra.rc8216@gmail.com>
2024-07-17 00:30:32 -0700

    buttons updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,16 +9,22 @@ const Hero = () => (
   <section className="relative flex flex-col px-12 py-12 min-h-[291px] max-md:px-5">
     <img loading="lazy" src={bgimage} className="absolute inset-0 object-cover w-full h-full" alt="Hero Background" />
     <div className="relative flex justify-between">
-      <img loading="lazy" src={backArrow} className="shrink-0 w-11 my-auto" alt="Back Arrow" />
+      <button>
+        <img loading="lazy" src={backArrow} className="shrink-0 w-11 my-auto" alt="Back Arrow" />
+      </button>
       <div className="flex gap-5">
-        <img loading="lazy" src={favIcon} className="shrink-0 w-[53px]" alt="Share Icon" />
-        <img loading="lazy" src={shareIcon} className="shrink-0 w-[53px]" alt="Favorite Icon" />
+        <button>
+          <img loading="lazy" src={favIcon} className="shrink-0 w-[53px]" alt="Share Icon" />
+        </button>
+        <button>
+          <img loading="lazy" src={shareIcon} className="shrink-0 w-[53px]" alt="Favorite Icon" />
+        </button>
       </div>
     </div>
-    <div className="relative flex self-end gap-4 px-3 py-3 mt-24 text-base font-medium text-red-400 bg-white rounded-lg max-md:mt-auto">
+    <button className="relative flex self-end gap-4 px-3 py-3 mt-24 text-base font-medium text-red-400 bg-white rounded-lg max-md:mt-auto">
       <img loading="lazy" src={imageIcon} className="shrink-0 w-7" alt="PhotoIcon" />
       <div className="my-auto">All (45) Photos</div>
-    </div>
+    </button>
   </section>
 );
 
